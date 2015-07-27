04e8240eab988075b2c95d007b3ebafbe6ec74fb
unknown <banerjee.soumyajyoti4@gmail.com>
2015-07-27 18:20:30 +0530

    class distribution defined

--- src/TrackingRecords.java
+++ src/TrackingRecords.java
@@ -1,4 +1,27 @@
+import java.util.ArrayList;
+
 
 public class TrackingRecords {
+	
+	ArrayList recordlist = new ArrayList();
 
+	public void insertRecord(Trackingcode newRecord)
+	{
+		
+	}
+	
+	public void splitRecordList(Range part1, Range part2)
+	{
+		
+	}
+	
+	public void mergeRecordList(Range part1, Range part2)
+	{
+		
+	}
+	
+	public void displayRecordList()
+	{
+		
+	}
 }
